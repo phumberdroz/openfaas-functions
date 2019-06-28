11ee7e92f4347098a1b64c73924d1559a4df5aca
Pierre Humberdroz <pierre.humberdroz@prezero.com>
2019-06-28 22:28:46 +0200

    fix database migration error

--- helm-repos/knexfile.js
+++ helm-repos/knexfile.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 const pg = require('pg');
 
-const url = process.env.DATABASE_URI || fs.readFileSync('/var/openfaas/secrets/database-uri', 'utf8').split('?')[0];
+let migrationsDir = './db/migrations';
+let seedsDir = './db/seeds';
+let url = process.env.DATABASE_URI;
+
 if (!process.env.DATABASE_URI) {
   pg.defaults.ssl = true;
+  seedsDir = './functions/db/seeds';
+  migrationsDir = './functions/db/seeds';
+  [url] = fs.readFileSync('/var/openfaas/secrets/database-uri', 'utf8').split('?');
 }
 
 console.log(`Connecting to ${url.replace(/:[^:]+@/, ':***@')}`);
@@ -11,9 +17,9 @@ module.exports = {
   client: 'pg',
   connection: url,
   migrations: {
-    directory: './db/migrations',
+    directory: migrationsDir,
   },
   seeds: {
-    directory: './db/seeds',
+    directory: seedsDir,
   },
 };
